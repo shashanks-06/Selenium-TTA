4e3abe24b3f99e208c2c2394a9bca9c63dfa63db
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-10-16 17:19:42 +0530

    Learning Selenium

--- src/test/java/Shashank/WebAutomation/ex_Selenium/Selenium03.java
+++ src/test/java/Shashank/WebAutomation/ex_Selenium/Selenium03.java
@@ -1,5 +1,30 @@
 package Shashank.WebAutomation.ex_Selenium;
 
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
+
 public class Selenium03 {
 
+    @Test
+    public void testMethod01() {
+        // EdgeOptions -> It will help you set the browser
+        // options to browsers
+        // window - size
+        // headless mode - there is not UI -> advantage - Fast Execution
+        // full UI mode - default - UI browser
+        // incognito mode - switch
+        // start Max
+        // add extensions - browsers
+        // 100 + others , https , http
+        // localstorage, download ?
+
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("--start-maximized");
+//        edgeOptions.addArguments("--window-size=800,600");
+        edgeOptions.addArguments("--incognito");
+
+        EdgeDriver edgeDriver = new EdgeDriver(edgeOptions);
+        edgeDriver.get("https://www.google.com");
+    }
 }
