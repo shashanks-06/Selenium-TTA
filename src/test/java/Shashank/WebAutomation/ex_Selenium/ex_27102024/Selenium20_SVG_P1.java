25fff0ce0555cca2662bef5f6e0c14b9ed359969
Shashank Surjekar <shashanksurjekar@gmail.com>
2024-10-31 14:08:54 +0530

    Handling SVG

---
+++ src/test/java/Shashank/WebAutomation/ex_Selenium/ex_27102024/Selenium20_SVG_P1.java
@@ -0,0 +1,57 @@
+package Shashank.WebAutomation.ex_Selenium.ex_27102024;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Selenium20_SVG_P1 {
+    WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser(){
+        EdgeOptions options = new EdgeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--start-maximized");
+        options.addArguments("guest");
+        driver = new EdgeDriver(options);
+    }
+
+    @Description("Verify that the after search, results are visible.")
+    @Test
+    public void test_flipkartSearch(){
+
+        driver.get("https://www.flipkart.com");
+
+        // Step 1 - Enter the "macmini" in the inputbox.
+        driver.findElement(By.name("q")).sendKeys("macmini");
+
+        // Step 2 - Click on the svg element.
+
+        List<WebElement> svgElements = driver.findElements(By.xpath("//*[name()='svg']"));
+//        //*[local-name()='svg']
+        svgElements.get(0).click();
+
+        List<WebElement> titlesResults = driver.findElements(By.xpath("//div[contains(@data-id,'CPU')]/div/a[2]"));
+        for (WebElement title : titlesResults){
+            System.out.println(title.getText());
+        }
+
+    }
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
+}
+
---
+++ src/test/java/Shashank/WebAutomation/ex_Selenium/ex_27102024/Selenium21_SVG_P2.java
@@ -0,0 +1,61 @@
+package Shashank.WebAutomation.ex_Selenium.ex_27102024;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Selenium21_SVG_P2 {
+
+WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser(){
+        EdgeOptions options = new EdgeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+        options.addArguments("--start-maximized");
+        options.addArguments("guest");
+        driver = new EdgeDriver(options);
+    }
+
+    @Description("Verify that the tripura is in india and click on it.")
+    @Test
+    public void test_svgIndiaMapAndClick(){
+        driver.get("https://www.amcharts.com/svg-maps/?map=india");
+
+        // local-name() , name()- Xpath (built in)
+        // document.querySelectorAll("#login-username"); - JS
+
+        List<WebElement> allStates = driver.findElements(By.xpath(
+                "//*[name()='svg']/*[name()='g'][7]/*[name()='g']/*[name()='g']/*[name()='path']"
+        ));
+
+        for (WebElement state : allStates){
+            System.out.println(state.getAttribute("aria-label"));
+
+//            To click on Tripura
+            if (Objects.requireNonNull(state.getAttribute("aria-label")).contains("Tripura")){
+                state.click();
+            }
+        }
+
+    }
+
+
+
+    @AfterTest
+    public void closeBrowser() throws InterruptedException {
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
+}
