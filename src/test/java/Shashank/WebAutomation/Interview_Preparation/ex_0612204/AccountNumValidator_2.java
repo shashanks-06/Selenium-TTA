391cb17edea490d6cbbf4b298f81af9c43705a5d
shashanks-06 <surjekarshashank@gmail.com>
2024-12-06 21:34:33 +0530

    Alphanumeric String to clean String

--- src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_0612204/AccountNumValidator_2.java
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_0612204/AccountNumValidator_2.java
@@ -4,7 +4,6 @@ public class AccountNumValidator_2 {
     public static void main(String[] args) {
 
         System.out.println(getCleanedNum("123-456-789"));
-
     }
 
     public static String getCleanedNum(String accountNum){
