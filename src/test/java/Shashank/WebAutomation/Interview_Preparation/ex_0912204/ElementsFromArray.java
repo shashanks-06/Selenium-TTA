b0086cc558516a7b0939a7d3ad8836d27577320b
shashanks-06 <surjekarshashank@gmail.com>
2024-12-09 16:09:41 +0530

    Sorting Array

---
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_0912204/ElementsFromArray.java
@@ -0,0 +1,55 @@
+package Shashank.WebAutomation.Interview_Preparation.ex_0912204;
+
+import java.util.Arrays;
+
+public class ElementsFromArray {
+
+    public static void descSortArray(int[] array){
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+            for (int j = 0; j < array.length - 1; j++) {
+
+                if(array[j] < array[j + 1]){
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static int getLargestOfArray(int[] array){
+        descSortArray(array);
+
+        return array[0];
+    }
+
+    public static int getSecondLargestOfArray(int[] array){
+        descSortArray(array);
+
+        return array[1];
+    }
+
+    public static int getSmallestOfArray(int[] array){
+        descSortArray(array);
+
+        int lastIndex = array.length - 1;
+
+        return array[lastIndex];
+    }
+
+
+    public static void main(String[] args) {
+        int[]array = {4, 6, 1, 9, 2, 3, 7, 5, 0};
+        descSortArray(array);
+        System.out.println("Descending Sorted Array" + Arrays.toString(array));
+
+        System.out.println("Largest Element of Array : " + getLargestOfArray(array));
+
+        System.out.println("Second Largest Element of Array : " + getSecondLargestOfArray(array));
+
+        System.out.println("Smallest Element of Array : " + getSmallestOfArray(array));
+    }
+
+}
