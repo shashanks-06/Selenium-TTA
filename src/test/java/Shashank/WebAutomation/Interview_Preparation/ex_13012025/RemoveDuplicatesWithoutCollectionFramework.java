2059609db409ba641d513f5a0b8b9ea01c827fdb
shashanks-06 <surjekarshashank@gmail.com>
2025-01-13 13:48:36 +0530

    remove duplicates without HashSet

---
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_13012025/RemoveDuplicatesWithoutCollectionFramework.java
@@ -0,0 +1,51 @@
+package Shashank.WebAutomation.Interview_Preparation.ex_13012025;
+
+import java.util.Arrays;
+
+// Write Java code to remove duplicate elements from array without using CollectionFramework
+public class RemoveDuplicatesWithoutCollectionFramework {
+
+    public static int[] removeDuplicates(int[] arr){
+
+        int n = arr.length;
+
+        if (n == 0 || n == 1) {
+            return arr;
+        }
+
+        int[] temp = new int[n];
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            boolean isDuplicate = false;
+
+            for (int j = 0; j < index; j++) {
+
+                if (arr[i] == temp[j]){
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (!isDuplicate){
+                temp[index++] = arr[i];
+            }
+        }
+
+        int[] result =  new int[index];
+        System.arraycopy(temp, 0, result, 0, index);
+
+
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 2, 4, 5, 3, 6, 4};
+        int[] result = removeDuplicates(array);
+
+        System.out.println(Arrays.toString(result));    // [1, 2, 3, 4, 5, 6]
+    }
+
+}
