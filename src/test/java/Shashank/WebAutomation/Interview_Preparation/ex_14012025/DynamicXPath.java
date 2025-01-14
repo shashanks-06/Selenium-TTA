69f6d91362bb6eb18da5c38732f5a2b74ef4f26d
shashanks-06 <surjekarshashank@gmail.com>
2025-01-14 13:28:16 +0530

    using dynamic xpath

---
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_14012025/DynamicXPath.java
@@ -0,0 +1,51 @@
+package Shashank.WebAutomation.Interview_Preparation.ex_14012025;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class DynamicXPath {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        EdgeOptions options = new EdgeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("guest");
+
+        driver = new EdgeDriver(options);
+
+        driver.get("https://register.rediff.com/register/register.php?FormName=user_details");
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void test_checkAvailabilityBtn(){
+        WebElement checkBtn = driver.findElement(By.xpath(
+                "//input[contains(@value, 'Check availability')]"));
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(checkBtn));
+
+        checkBtn.click();
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
+
+}
