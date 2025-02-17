335e5ebbc7929c192080f4a15db7545b583944b0
shashanks-06 <surjekarshashank@gmail.com>
2025-02-17 17:33:39 +0530

    kth element found

---
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/02February/ex_17022025/FindKthElement.java
@@ -0,0 +1,23 @@
+package Shashank.WebAutomation.Interview_Preparation._02February.ex_17022025;
+
+import java.util.Arrays;
+
+public class FindKthElement {
+
+    public static int kthSmallestElement(int[] array, int k){
+        Arrays.sort(array);
+        return array[k - 1];
+    }
+
+    public static int kthBiggestElement(int[] array, int k){
+        Arrays.sort(array);
+        return array[array.length - k];
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 2, 4, 7, 1, 3, 6};
+        System.out.println("3rd Smallest Element: " + kthSmallestElement(array, 3)); // 3
+        System.out.println("3rd Biggest Element: " + kthBiggestElement(array, 3));  // 5
+    }
+
+}
