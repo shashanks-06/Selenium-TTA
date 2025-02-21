749997f61c573b8de81f46ac431425255cb3bb54
shashanks-06 <surjekarshashank@gmail.com>
2025-02-21 16:55:42 +0530

    element added to desired pos in arr

---
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_2025_02_Feb/ex_21022025/InsertElementInArray_1.java
@@ -0,0 +1,38 @@
+package Shashank.WebAutomation.Interview_Preparation.ex_2025_02_Feb.ex_21022025;
+
+import java.util.Arrays;
+
+public class InsertElementInArray_1 {
+
+    public static int[] insertElement(int[] array, int index, int valueToInsert){
+
+        if (index < 0 || index >= array.length + 1){
+            System.out.println("Index " + index + " is out of bounds");
+            return new int[]{};
+        }
+
+        int[] newArray = new int[array.length + 1];
+
+        for (int i = 0; i < index; i++) {
+            newArray[i] = array[i];
+        }
+
+        newArray[index] = valueToInsert;
+
+        for (int i = index; i < array.length ; i++) {
+            newArray[i+1] = array[i];
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        int value = 25;
+        int index = 2;
+
+        int[] result = insertElement(arr, index, value);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
