364d88d5c1802565939d225e741976acc9ec251c
shashanks-06 <surjekarshashank@gmail.com>
2025-02-26 23:32:01 +0530

    reversed words

--- src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
@@ -17,40 +17,27 @@ public class Practice {
 //        Thread.sleep(2000);
 //    }
 
-    public static void reverse(int[] arr, int start, int end){
-
-        while (start < end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-
-            start++;
-            end--;
+    public static String reverse(String str){
+        if (str == null || str.isEmpty()){
+            System.out.println("Invalid");
         }
+
+        assert str != null;
+        String[] words = str.split(" ");
+
+        StringBuilder result = new StringBuilder();
+
+        for (String word : words){
+            result.append(new StringBuilder(word).reverse()).append(" ");
+        }
+
+        return result.toString().trim();
+
     }
 
-    public static void rightShiftArray(int[] arr, int k){
-        int n = arr.length;
-
-        reverse(arr, 0, n - 1);
-        reverse(arr,0, k - 1);
-        reverse(arr, k, n -1);
-    }
-
-    public static void leftShiftArray(int[] arr, int k){
-        int n = arr.length;
-
-        reverse(arr, 0, k - 1);
-        reverse(arr, k, n-1);
-        reverse(arr,0, n-1);
-    }
 
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
-        int steps = 3;
-//        rightShiftArray(arr, steps);
-        leftShiftArray(arr, steps);
-        System.out.println(Arrays.toString(arr));
+        System.out.println(reverse("Test Automation"));
     }
 
 
