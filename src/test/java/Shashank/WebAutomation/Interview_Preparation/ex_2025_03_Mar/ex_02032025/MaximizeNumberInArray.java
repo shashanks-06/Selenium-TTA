295074744982b8a807b4f5636bda8def2896f72c
shashanks-06 <surjekarshashank@gmail.com>
2025-03-02 23:08:38 +0530

    maximized int of array

--- src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_2025_03_Mar/ex_02032025/MaximizeNumberInArray.java
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_2025_03_Mar/ex_02032025/MaximizeNumberInArray.java
@@ -7,9 +7,13 @@ public class MaximizeNumberInArray {
     public static void maximizeNumber(int[] array){
         int n = array.length;
 
+        // Identify the rightmost digit that can be incremented (not a 9)
         for (int i = n - 1; i >= 0 ; i--) {
+
             if (array[i] != 9){
-             array[i]++;
+             array[i]++;    // Increment the rightmost non-9 digit
+
+                // Set all subsequent digits to 0
                 for (int j = i + 1; j < n ; j++) {
                     array[j] = 0;
                 }
@@ -20,8 +24,8 @@ public class MaximizeNumberInArray {
 
     public static void main(String[] args) {
         int[] array = {1, 2, 9};
-        maximizeNumber(array);
-        maximizeNumber(array);
+        maximizeNumber(array);  // [1, 3, 0]
+        maximizeNumber(array);  // [1, 3, 1]
 
         System.out.println(Arrays.toString(array));
     }
