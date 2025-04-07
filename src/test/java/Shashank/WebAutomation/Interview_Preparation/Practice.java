e828c4c4d55a21e723592d4871cc3ea96803d26d
shashanks-06 <surjekarshashank@gmail.com>
2025-04-07 16:59:14 +0530

    printed factorial

--- src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
@@ -1,76 +1,31 @@
 package Shashank.WebAutomation.Interview_Preparation;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
 
-import java.time.Duration;
-import java.util.List;
 
 public class Practice {
 
-    WebDriver driver;
+  public static int getFactorial(int num){
+      if(num == 0 || num == 1) return 1;
+      if(num < 0) return -1;
 
-    @BeforeTest
-    public void setUp(){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
-        options.addArguments("--guest");
+      int result = 1;
+//      for (int i = 1; i <= num; i++){
+//          result *= i;
+//      }
+      int i = 2;
+      while (i <= num){
+          result *= i;
+          i++;
+      }
 
-        driver = new ChromeDriver(options);
-        driver.get("https://www.amazon.in");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      return result;
+  }
+
+    public static void main(String[] args) {
+        System.out.println(getFactorial(6));
     }
 
-    @Test
-    public void test_Amazon(){
 
-        Actions act = new Actions(driver);
 
-        WebElement accountsAndListsElement = driver.findElement(By.xpath(
-                "//div[@class=\"nav-line-1-container\"]"));
-
-        act.moveToElement(accountsAndListsElement).perform();
-
-        WebElement signInBtnElement = driver.findElement(By.className("nav-action-inner"));
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOf(signInBtnElement));
-
-        String yourListsHeading = driver.findElement(By.id("nav-al-title")).getText();
-        List<WebElement> yourLists = driver.findElements(By.xpath("//div[@id=\"nav-al-wishlist\"]//span"));
-
-        System.out.println("\"" + yourListsHeading + "\" :");
-        for (WebElement listItem : yourLists){
-            System.out.println(listItem.getText());
-        }
-
-        System.out.println("=====================================================");
-
-        String yourAccountsHeading = driver.findElement(By.xpath(
-                "//div[@id=\"nav-al-your-account\"]//div")).getText();
-        List<WebElement> yourAccounts = driver.findElements(By.xpath(
-                "//div[@id=\"nav-al-your-account\"]//span"));
-
-        System.out.println("\"" + yourAccountsHeading + "\" :");
-        for (WebElement listItem : yourAccounts){
-            System.out.println(listItem.getText());
-        }
-
-    }
-
-    @AfterTest
-    public void tearDown() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.quit();
-    }
 
 }
