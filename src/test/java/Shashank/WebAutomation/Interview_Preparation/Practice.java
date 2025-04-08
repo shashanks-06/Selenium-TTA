3839b6ebe7b0697f203e3b9d8e9471878bb342ab
shashanks-06 <surjekarshashank@gmail.com>
2025-04-08 17:20:57 +0530

    moved neg ele to start in array

--- src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
@@ -1,44 +1,27 @@
 package Shashank.WebAutomation.Interview_Preparation;
 
 import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+
 
 public class Practice {
 
-    public static void reverse(int[] array, int start, int end) {
-        while(start < end){
-            int temp = array[start];
-            array[start] = array[end];
-            array[end] = temp;
+    public static void negativeToStart(int[] array) {
+        int left = 0;
 
-            start++;
-            end--;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0){
+                int temp = array[i];
+                array[i] = array[left];
+                array[left++] = temp;
+            }
         }
-    }
-
-    public static void leftShiftArray(int[] array, int k) {
-        int n = array.length;
-
-        reverse(array, 0, k - 1);
-        reverse(array, k, n-1);
-        reverse(array, 0, n-1);
-    }
-
-    public static void rightShiftArray(int[] array, int k){
-        int n = array.length;
-
-        reverse(array, 0, n-1);
-        reverse(array, 0, k-1);
-        reverse(array, k, n-1);
     }
 
 
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5};
+        int[] array = {-1, 2, -3, 4, -5};
+        negativeToStart(array);
 
-//        leftShiftArray(array, 3);
-        rightShiftArray(array, 3);
         System.out.println(Arrays.toString(array));
 
     }
