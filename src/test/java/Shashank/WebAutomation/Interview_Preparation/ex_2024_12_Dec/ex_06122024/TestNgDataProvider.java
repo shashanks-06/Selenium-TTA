8e90bb08dfc511ba513773c4148b428bd0914b34
shashanks-06 <surjekarshashank@gmail.com>
2025-04-08 15:54:01 +0530

    login with Data Provider

--- src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_2024_12_Dec/ex_06122024/TestNgDataProvider.java
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/ex_2024_12_Dec/ex_06122024/TestNgDataProvider.java
@@ -1,9 +1,7 @@
 package Shashank.WebAutomation.Interview_Preparation.ex_2024_12_Dec.ex_06122024;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
