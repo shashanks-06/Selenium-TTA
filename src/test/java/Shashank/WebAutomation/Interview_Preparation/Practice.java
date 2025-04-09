76a29a31a9ec3e4fbf0ef279e7c0e3c609dc2fac
shashanks-06 <surjekarshashank@gmail.com>
2025-04-09 11:53:56 +0530

    counted occurrences of characters

--- src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
@@ -17,10 +17,27 @@ public class Practice {
         }
     }
 
+    public static void countOccurrences(String string) {
+        char[] charArray = string.toLowerCase().toCharArray();
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : charArray){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] array = {1,1,3, 2,2, 3,3,4, 4,5,5, 5};
-       countOccurrences(array);
+//       countOccurrences(array);
+
+        String string = "aabbczcddeeffggh";
+        countOccurrences(string);
     }
 
 }
