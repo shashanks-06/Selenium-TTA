24893f76079fdd4a7619f5cbfb5639bd8575ff57
shashanks-06 <surjekarshashank@gmail.com>
2025-04-11 17:16:34 +0530

    printed common options of 2 dropdowns

--- src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
@@ -1,38 +1,114 @@
 package Shashank.WebAutomation.Interview_Preparation;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 import java.util.*;
 
 
 public class Practice {
 
-    public static int[] findSubArray(int[] array, int expectedSum) {
-        int currentSum = 0;
-        int start = 0;
+    WebDriver driver;
+    Select select;
 
-        for (int end = 0; end < array.length; end++) {
+    @BeforeTest
+    public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("guest");
 
-            currentSum += array[end];
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
 
-            while(currentSum > expectedSum && start <= end){
-                currentSum -= array[start];
-                start++;
-            }
+    public void open1stWebsite(){
+        driver.get("https://www.globalsqa.com/demo-site/select-dropdown-menu/");
+    }
 
-            if (currentSum == expectedSum){
-                System.out.println("SubArray found in between : " + start + " and " + end);
-                return Arrays.copyOfRange(array, start, end + 1);
-            }
+    public void open2ndWebsite(){
+        String url = "https://practice.expandtesting.com/dropdown";
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.open(arguments[0], '_blank');", url);
+    }
+
+    public List<String> get1stWebOptions(){
+        WebElement dropdownElement = driver.findElement(By.xpath(
+                "//div[@rel-title=\"Select Country\"]//select"));
+
+        select = new Select(dropdownElement);
+        List<String> list = new ArrayList<>();
+
+        for (WebElement option : select.getOptions()){
+            list.add(option.getText());
         }
 
-        return new int[]{};
+        return list;
     }
 
 
-    public static void main(String[] args) {
-        int[] numbers = {1, 4, 20, 3, 10, 5};
-        int expectedSum = 33;
+    public List<String> get2ndWebOptions(){
+        WebElement dropdownElement = driver.findElement(By.id("country"));
 
-        System.out.println(Arrays.toString(findSubArray(numbers, expectedSum)));
+        select = new Select(dropdownElement);
+
+        List<String> list = new ArrayList<>();
+
+        for (WebElement options : select.getOptions()){
+            list.add(options.getText());
+        }
+
+        return list;
     }
+
+    public List<String> windowHandler(){
+        Set<String> allWindowHandles = driver.getWindowHandles();
+        Iterator<String> itr = allWindowHandles.iterator();
+        String mainTab = itr.next();
+        String newTab = itr.next();
+
+        List<String> windows = new ArrayList<>();
+        windows.add(mainTab);
+        windows.add(newTab);
+
+        return windows;
+    }
+
+    @Test
+    public void test_commonOptions(){
+        open1stWebsite();
+
+        List<String> list1 = get1stWebOptions();
+
+        open2ndWebsite();
+
+        driver.switchTo().window(windowHandler().get(1));
+
+        List<String> list2 = get2ndWebOptions();
+
+        list1.retainAll(list2);
+
+        System.out.println("Common options in between the 2 Dropdowns : ");
+        System.out.println(list1);
+    }
+
+    @AfterTest
+    public void tearDown() throws InterruptedException{
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+
+
+
 
 }
