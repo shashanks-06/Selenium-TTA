7215196f82bab7c93ed97048487aa9e512839578
shashanks-06 <surjekarshashank@gmail.com>
2025-04-12 17:26:37 +0530

    printed ele without duplicates

--- src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
+++ src/test/java/Shashank/WebAutomation/Interview_Preparation/Practice.java
@@ -1,17 +1,5 @@
 package Shashank.WebAutomation.Interview_Preparation;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import java.time.Duration;
 import java.util.*;
 
 
